078767b62d78e353d3790407934d001ad7b4549c
DuyNQ <duynq@omt.vn>
2023-04-08 15:31:57 +0700

    biding data from controller to vue

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,5 +1,18 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+defineProps({
+  message: String
+})
+
+</script>
+
 <template>
   <div>
-    Index
+    Index 1
+  </div>
+  <Link href="/show">Show page</Link>
+  <div>
+    <p>The message is {{message}}</p>
   </div>
 </template>
+
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,5 +1,11 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+</script>
+
 <template>
   <div>
-    Show
+    Show 1
   </div>
+  <Link href="/">Index page</Link>
 </template>
+
