0e6065de6c06adbcef7eab18d21803314dbba035
DuyNQ <duynq@omt.vn>
2023-05-12 17:49:59 +0700

    install packages support laravel

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
+      dd(Listing::where('beds', '>', 4)->orWhere('city', 'Lake Harleytown')->orderBy('beds')->select('city', 'code', 'street as Thành phố')->get());
         return inertia(
             'Index/Index',
             [
@@ -15,7 +17,7 @@ class IndexController extends Controller
             ]
         );
     }
-    
+
     public function show()
     {
         return inertia('Index/Show');
