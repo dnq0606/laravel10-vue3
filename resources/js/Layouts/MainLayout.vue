ff47d18565ee0095d58a7269935ea5dd9b28d3d4
duynq <duynq3.6@gmail.com>
2023-05-16 23:29:14 +0700

    install tailwindcss

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,6 +3,23 @@
     <Link href="/listing">Listings</Link>&nbsp;
     <Link href="/listing/create">Add new listing</Link>
   </div>
+
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800">
+    <div class="container mx-auto">
+      <nav class="flex justify-between items-center p-4">
+        <div class="text-lg font-medium">
+          <Link href="/listing">Listings</Link>
+        </div>
+        <div class="text-lg font-medium text-indigo-800">
+          <Link href="/listing">LazaZillow</Link>
+        </div>
+        <div class="text-lg font-medium">
+          <Link href="/listing/create">Add new listing</Link>
+        </div>
+      </nav>
+    </div>
+  </header>
+
   <content>
     <div v-if="flashSuccess" class="success">
       {{ flashSuccess }}
@@ -19,13 +36,3 @@ import { computed } from 'vue'
 const page = usePage()
 const flashSuccess = computed(() => page.props.flash.success)
 </script>
-
-<style scoped>
-.success {
-  background-color: green;
-  color: white;
-  padding: 6px;
-  margin-bottom: 10px;
-  border-radius: 4px;
-}
-</style>
